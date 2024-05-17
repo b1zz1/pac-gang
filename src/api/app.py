3e24fe2ce627dceda370695303e303b1a3c2e3b5
MIGUEL RICARDO BUTTENDORF <miguel.buttendorf@catolicasc.edu.br>
2024-05-17 19:48:00 -0300

    Updated app.py

--- src/api/app.py
+++ src/api/app.py
@@ -1,3 +1,5 @@
+# Feito por Miguel Ricardo Buttendorf
+
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
