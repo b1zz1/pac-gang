8b803e318ec68d773748fd2e92d6301db13bba4f
Luiz Almeida Júnior <e-luizj@weg.net>
2024-05-18 13:48:10 -0300

    Architecture
    Some requirements and structure adjusments

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +10,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="pt-br">
+      <body className={montserrat.className}>
         <main>{children}</main>
       </body>
     </html>
