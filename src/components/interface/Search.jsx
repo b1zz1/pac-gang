5124c39d442cd6cbffa86f6b2b82000e8ce5d109
ALenckulh <127156754+ALenckulh@users.noreply.github.com>
2024-05-20 18:31:46 -0300

    Mudança de Biblioteca

--- src/components/interface/Search.jsx
+++ src/components/interface/Search.jsx
@@ -1,26 +1,17 @@
 "use client";
 
 import React from "react";
-import { Input } from "@mantine/core";
+import Input from "antd/es/input/Input";
 import { PiMagnifyingGlass } from "react-icons/pi";
 
 const Search = () => {
   return (
     <div className="w-full">
       <Input
-        className="w-full"
-        variant="filled"
         placeholder="Search..."
-        leftSection={<PiMagnifyingGlass className="text-purple.6" />}
-        styles={(theme) => ({
-          input: {
-            color: theme.colors.primaryColor[5],
-            backgroundColor: theme.colors.primaryColor[2],
-            '::placeholder': {
-              text: theme.colors.primaryColor[2],
-            },
-          },
-        })}
+        variant="filled"
+        size="large"
+        className="bg-purple.3 hover:bg-purple.3 focus:bg-purple.3 focus:border-purple.6 text-purple.6 placeholder-purple.6"
       />
     </div>
   );
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,32 +1,14 @@
 'use client'
 
-import { useDisclosure } from '@mantine/hooks'
-
-import { Group, Burger, Image, Text, ActionIcon } from '@mantine/core'
 import { IconContext } from "react-icons";
 import { PiUserFill } from 'react-icons/pi'
 
 const Header = () => {
-    const [opened, { toggle }] = useDisclosure()
 
     return (
-        <Group h="100%" ml={"2rem"} mr={"2rem"} justify="space-between" align="center" ta={"center"}>
-            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-            <Group>
-                <Image
-                    radius="md"
-                    h={24}
-                    w={24}
-                    src="coffee.svg"
-                    fallbackSrc="#"
-                    alt="Logo"
-                />
-                <Text c="primaryColor" fw={400} size="xl">Repair Café</Text>
-            </Group>
-            <ActionIcon variant={"filled"} color="primaryColor" size="lg" radius="xl">
-                    <PiUserFill style={{width: '60%', height: '60%'}} />
-            </ActionIcon>
-        </Group>
+        <div className="h-12 w-full flex justify-center items-center bg-white.1 text-purple.6">
+            Header
+        </div>
     )
 }
 
